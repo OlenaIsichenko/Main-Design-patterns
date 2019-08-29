effbb6f81306a2a3fbcd20a7e364c74efbe87174
OlenaIsichenko <47576497+OlenaIsichenko@users.noreply.github.com>
2019-08-29 23:06:55 +0300

    Create Subscription.php

---
+++ Observer/Subscription.php
@@ -0,0 +1,30 @@
+<?php
+
+class Subscription implements Observer  
+{
+    private $subscribers;
+		
+    public function __construct() 
+    {
+        $this->subscribers = [];
+    }
+    public function subscribe(Subject $subscriber) 
+    {
+	      array_push($this->subscribers, $subscriber);
+	      print_r("You are subscribed :) </br>");
+    }
+    
+    public function unsubscribe(Subject $subscriber) 
+    {
+	      array_pop($this->subscribers);
+	      print_r("You are unsubscribed :( </br>" );
+    }
+    
+    public function notify()  
+    {
+	      foreach ($this->subscribers as $subscriber) {
+            $subscriber->update();
+	          print_r("We have just notified the subscriber. </br>");
+        }
+    }
+}
